fe35531d21710e2f4f9f4a7590bcc0e997f2de3e
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-10-08 15:06:34 +0200

    four_of_a_kind as an instance method

--- java/src/main/java/org/codingdojo/yatzy1/Yatzy1.java
+++ java/src/main/java/org/codingdojo/yatzy1/Yatzy1.java
@@ -156,14 +156,18 @@ public class Yatzy1 {
             return 0;
     }
 
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5) {
+    public static int four_of_a_kind(int d1, int d2, int d3, int d4, int d5) {
+        return new Yatzy1(d1, d2, d3, d4, d5).four_of_a_kind();
+    }
+
+    public int four_of_a_kind() {
         int[] tallies;
         tallies = new int[6];
-        tallies[_1 - 1]++;
-        tallies[_2 - 1]++;
-        tallies[d3 - 1]++;
-        tallies[d4 - 1]++;
-        tallies[d5 - 1]++;
+        tallies[dice[0] - 1]++;
+        tallies[dice[1] - 1]++;
+        tallies[dice[2] - 1]++;
+        tallies[dice[3] - 1]++;
+        tallies[dice[4] - 1]++;
         for (int i = 0; i < 6; i++)
             if (tallies[i] >= 4)
                 return (i + 1) * 4;
