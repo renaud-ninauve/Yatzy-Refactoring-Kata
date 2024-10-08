cc6e74a4440fcf0510fd10d8224f814b61d0326d
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-10-08 14:57:55 +0200

    ones as an instance method

--- java/src/main/java/org/codingdojo/yatzy1/Yatzy1.java
+++ java/src/main/java/org/codingdojo/yatzy1/Yatzy1.java
@@ -30,14 +30,17 @@ public class Yatzy1 {
         return 0;
     }
 
-
     public static int ones(int d1, int d2, int d3, int d4, int d5) {
+        return new Yatzy1(d1, d2, d3, d4, d5).ones();
+    }
+
+    public int ones() {
         int sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        if (d4 == 1) sum++;
-        if (d5 == 1)
+        if (dice[0] == 1) sum++;
+        if (dice[1] == 1) sum++;
+        if (dice[2] == 1) sum++;
+        if (dice[3] == 1) sum++;
+        if (dice[4] == 1)
             sum++;
 
         return sum;
