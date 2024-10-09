0df3b8b801ab152e29809de4a5656dc00084d372
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-10-09 10:02:55 +0200

    two_pair

--- java/src/main/java/org/codingdojo/yatzy1/Yatzy1.java
+++ java/src/main/java/org/codingdojo/yatzy1/Yatzy1.java
@@ -3,6 +3,7 @@ package org.codingdojo.yatzy1;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Yatzy1 {
 
@@ -147,23 +148,17 @@ public class Yatzy1 {
     }
 
     public int two_pair() {
-        int[] counts = new int[6];
-        counts[dice[0] - 1]++;
-        counts[dice[1] - 1]++;
-        counts[dice[2] - 1]++;
-        counts[dice[3] - 1]++;
-        counts[dice[4] - 1]++;
-        int n = 0;
-        int score = 0;
-        for (int i = 0; i < 6; i += 1)
-            if (counts[6 - i - 1] >= 2) {
-                n++;
-                score += (6 - i);
-            }
-        if (n == 2)
-            return score * 2;
-        else
+        Frequencies frequencies = Frequencies.from(Arrays.stream(dice).boxed().toList());
+        List<Integer> pairs = frequencies.findDiceValuesByMinFrequency(2);
+        if (pairs.size() < 2) {
             return 0;
+        }
+
+        return pairs.stream()
+            .sorted(Comparator.<Integer>naturalOrder().reversed())
+            .limit(2)
+            .mapToInt(i -> i * 2)
+            .sum();
     }
 
     public int four_of_a_kind() {
