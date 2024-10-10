d7dd51dad6c491f0fb1832174617b366e6ce1112
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-10-10 09:34:48 +0200

    extract methods

--- java/src/main/java/org/codingdojo/yatzy2/Yatzy2.java
+++ java/src/main/java/org/codingdojo/yatzy2/Yatzy2.java
@@ -29,162 +29,216 @@ public class Yatzy2 implements YatzyCalculator {
         int result;
         switch (category) {
             case CHANCE:
-
-                // chance sums the dice
-                result = dice.stream().mapToInt(Integer::intValue).sum();
+                result = scoreChance(dice);
                 break;
-
             case YATZY:
-
-                // score for yatzy if all dice are the same
-                int yatzyResult = 0;
-                if (diceFrequencies.containsValue(5)) {
-                    yatzyResult = 50;
-                }
-                result = yatzyResult;
+                result = scoreYatzy(diceFrequencies);
                 break;
-
             case ONES:
-                // sum all the ones
-                result = diceFrequencies.get(1);
+                result = scoreOnes(diceFrequencies);
                 break;
-
             case TWOS:
-                // sum all the twos
-                result = diceFrequencies.get(2) * 2;
+                result = scoreTwos(diceFrequencies);
                 break;
-
             case THREES:
-                // sum all the threes
-                result = diceFrequencies.get(3) * 3;
+                result = scoreThrees(diceFrequencies);
                 break;
-
             case FOURS:
-                // sum all the fours
-                result = diceFrequencies.get(4) * 4;
+                result = scoreFours(diceFrequencies);
                 break;
-
             case FIVES:
-                // sum all the fives
-                result = diceFrequencies.get(5) * 5;
+                result = scoreFives(diceFrequencies);
                 break;
-
             case SIXES:
-                // sum all the sixes
-                result = diceFrequencies.get(6) * 6;
+                result = scoreSixes(diceFrequencies);
                 break;
-
             case PAIR:
-
-                // score pair if two dice are the same
-                int pairResult = 0;
-                // score highest pair if there is more than one
-                for (int i : DICE_VALUES) {
-                    if (diceFrequencies.get(i) >= 2) {
-                        pairResult = i * 2;
-                        break;
-                    }
-                }
-                result = pairResult;
+                result = scorePair(diceFrequencies);
                 break;
-
             case THREE_OF_A_KIND:
-
-                // score if three dice are the same
-                int threeKindResult = 0;
-                for (int i : DICE_VALUES) {
-                    if (diceFrequencies.get(i) >= 3) {
-                        threeKindResult = i * 3;
-                        break;
-                    }
-                }
-                result = threeKindResult;
+                result = scoreThreeOfAKind(diceFrequencies);
                 break;
-
             case FOUR_OF_A_KIND:
-
-                // score if four dice are the same
-                int fourKindResult = 0;
-                for (int i : DICE_VALUES) {
-                    if (diceFrequencies.get(i) >= 4) {
-                        fourKindResult = i * 4;
-                        break;
-                    }
-                }
-                result = fourKindResult;
+                result = scoreRourOfAKind(diceFrequencies);
                 break;
-
             case SMALL_STRAIGHT:
-
-                // score if dice contains 1,2,3,4,5
-                int smallStraightResult = 0;
-                long count = 0L;
-                for (Integer frequency : diceFrequencies.values()) {
-                    if (frequency == 1) {
-                        count++;
-                    }
-                }
-                if (count == 5 && diceFrequencies.get(6) == 0) {
-                    for (Integer die : dice) {
-                        smallStraightResult += die;
-                    }
-                }
-                result = smallStraightResult;
+                result = scoreSmallStraight(dice, diceFrequencies);
                 break;
-
             case LARGE_STRAIGHT:
-
-                // score if dice contains 2,3,4,5,6
-                int largeStraightResult = 0;
-                long straightCount = 0L;
-                for (Integer frequency : diceFrequencies.values()) {
-                    if (frequency == 1) {
-                        straightCount++;
-                    }
-                }
-                if (straightCount == 5 && diceFrequencies.get(1) == 0) {
-                    for (Integer die : dice) {
-                        largeStraightResult += die;
-                    }
-                }
-                result = largeStraightResult;
+                result = scoreLargeStraight(dice, diceFrequencies);
                 break;
-
             case TWO_PAIRS:
-
-                // score if there are two pairs
-                int twoPairResult = 0;
-                long pairCount = 0L;
-                for (Integer frequency : diceFrequencies.values()) {
-                    if (frequency >= 2) {
-                        pairCount++;
-                    }
-                }
-                if (pairCount == 2) {
-                    for (int i : DICE_VALUES) {
-                        if (diceFrequencies.get(i) >= 2) {
-                            twoPairResult += i * 2;
-                        }
-                    }
-                }
-                result = twoPairResult;
+                result = scoreTwoPairs(diceFrequencies);
                 break;
-
             case FULL_HOUSE:
-
-                // score if there is a pair as well as three of a kind
-                int fullHouseResult = 0;
-                if (diceFrequencies.containsValue(2) && diceFrequencies.containsValue(3)) {
-                    for (Integer die : dice) {
-                        fullHouseResult += die;
-                    }
-                }
-                result = fullHouseResult;
+                result = scoreFullHouse(dice, diceFrequencies);
                 break;
-
             default:
                 result = 0;
         }
+        return result;
+    }
+
+    private static int scoreFullHouse(List<Integer> dice, Map<Integer, Integer> diceFrequencies) {
+        int result;
+        int fullHouseResult = 0;
+        if (diceFrequencies.containsValue(2) && diceFrequencies.containsValue(3)) {
+            for (Integer die : dice) {
+                fullHouseResult += die;
+            }
+        }
+        result = fullHouseResult;
+        return result;
+    }
+
+    private static int scoreTwoPairs(Map<Integer, Integer> diceFrequencies) {
+        int result;
+        // score if there are two pairs
+        int twoPairResult = 0;
+        long pairCount = 0L;
+        for (Integer frequency : diceFrequencies.values()) {
+            if (frequency >= 2) {
+                pairCount++;
+            }
+        }
+        if (pairCount == 2) {
+            for (int i : DICE_VALUES) {
+                if (diceFrequencies.get(i) >= 2) {
+                    twoPairResult += i * 2;
+                }
+            }
+        }
+        result = twoPairResult;
+        return result;
+    }
+
+    private static int scoreLargeStraight(List<Integer> dice, Map<Integer, Integer> diceFrequencies) {
+        int result;
+        int largeStraightResult = 0;
+        long straightCount = 0L;
+        for (Integer frequency : diceFrequencies.values()) {
+            if (frequency == 1) {
+                straightCount++;
+            }
+        }
+        if (straightCount == 5 && diceFrequencies.get(1) == 0) {
+            for (Integer die : dice) {
+                largeStraightResult += die;
+            }
+        }
+        result = largeStraightResult;
+        return result;
+    }
+
+    private static int scoreSmallStraight(List<Integer> dice, Map<Integer, Integer> diceFrequencies) {
+        int result;
+        int smallStraightResult = 0;
+        long count = 0L;
+        for (Integer frequency : diceFrequencies.values()) {
+            if (frequency == 1) {
+                count++;
+            }
+        }
+        if (count == 5 && diceFrequencies.get(6) == 0) {
+            for (Integer die : dice) {
+                smallStraightResult += die;
+            }
+        }
+        result = smallStraightResult;
+        return result;
+    }
+
+    private static int scoreRourOfAKind(Map<Integer, Integer> diceFrequencies) {
+        int result;
+        // score if four dice are the same
+        int fourKindResult = 0;
+        for (int i : DICE_VALUES) {
+            if (diceFrequencies.get(i) >= 4) {
+                fourKindResult = i * 4;
+                break;
+            }
+        }
+        result = fourKindResult;
+        return result;
+    }
+
+    private static int scoreThreeOfAKind(Map<Integer, Integer> diceFrequencies) {
+        int result;
+        int threeKindResult = 0;
+        for (int i : DICE_VALUES) {
+            if (diceFrequencies.get(i) >= 3) {
+                threeKindResult = i * 3;
+                break;
+            }
+        }
+        result = threeKindResult;
+        return result;
+    }
+
+    private static int scorePair(Map<Integer, Integer> diceFrequencies) {
+        int result;
+        // score pair if two dice are the same
+        int pairResult = 0;
+        // score highest pair if there is more than one
+        for (int i : DICE_VALUES) {
+            if (diceFrequencies.get(i) >= 2) {
+                pairResult = i * 2;
+                break;
+            }
+        }
+        result = pairResult;
+        return result;
+    }
+
+    private static int scoreSixes(Map<Integer, Integer> diceFrequencies) {
+        int result;
+        result = diceFrequencies.get(6) * 6;
+        return result;
+    }
+
+    private static int scoreFives(Map<Integer, Integer> diceFrequencies) {
+        int result;
+        result = diceFrequencies.get(5) * 5;
+        return result;
+    }
+
+    private static int scoreFours(Map<Integer, Integer> diceFrequencies) {
+        int result;
+        result = diceFrequencies.get(4) * 4;
+        return result;
+    }
+
+    private static int scoreThrees(Map<Integer, Integer> diceFrequencies) {
+        int result;
+        result = diceFrequencies.get(3) * 3;
+        return result;
+    }
+
+    private static int scoreTwos(Map<Integer, Integer> diceFrequencies) {
+        int result;
+        result = diceFrequencies.get(2) * 2;
+        return result;
+    }
+
+    private static int scoreOnes(Map<Integer, Integer> diceFrequencies) {
+        int result;
+        result = diceFrequencies.get(1);
+        return result;
+    }
+
+    private static int scoreYatzy(Map<Integer, Integer> diceFrequencies) {
+        int result;
+        int yatzyResult = 0;
+        if (diceFrequencies.containsValue(5)) {
+            yatzyResult = 50;
+        }
+        result = yatzyResult;
+        return result;
+    }
+
+    private static int scoreChance(List<Integer> dice) {
+        int result;
+        result = dice.stream().mapToInt(Integer::intValue).sum();
         return result;
     }
 
